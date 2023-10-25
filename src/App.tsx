961fe5852b319be8d79cfb6af7249d2752c42ff1
Yurii Shkrobut <georgebest2409@gmail.com>
2023-10-25 17:47:24 +0300

    Before Handle Typical Loading Cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-
-import usersFromServer from './api/users.json';
-
+import React, { useState, useEffect } from 'react';
 import { UsersList } from './components/UserList';
 import { User } from './types';
+import { getUsers } from './services/user';
+import { Blocks } from 'react-loader-spinner';
+
 
 export const App: React.FC = () => {
-  const [users, setUsers] = useState<User[]>(usersFromServer);
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    getUsers().then(setUsers)
+  }, [])
 
   return (
     <div className="section py-5">
@@ -14,6 +18,17 @@ export const App: React.FC = () => {
         <div className="title">Users</div>
 
         <UsersList users={users} />
+        <Blocks
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+        />
+
+        <p className="title is-5">There are no users</p>
+        <p className="notification is-danger">Try again later</p>
       </div>
     </div>
   );
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,1 +1,15 @@
-export {}
+import { User } from "../types";
+
+export function getUsers(): Promise<User[]> {
+  return fetch('http://localhost:3000/api/users.json')
+    .then((response) => {
+      if (!response.ok) {
+
+      }
+
+      return response.json();
+    })
+
+    .then(users => users.slice(0, 3)
+    )
+}
