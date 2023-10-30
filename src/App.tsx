799ed22c392a1f4ce7b982a09dc23761054ded05
Yurii Shkrobut <georgebest2409@gmail.com>
2023-10-30 11:59:05 +0200

    Before Http client

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,58 @@ import { User } from './types';
 import { getUsers } from './services/user';
 import { Blocks } from 'react-loader-spinner';
 
-
 export const App: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [updateAt, setUpdateAt] = useState(new Date());
 
   useEffect(() => {
-    getUsers().then(setUsers)
-  }, [])
+    setLoading(true);
+
+    setTimeout(() => {
+      getUsers()
+        .then(setUsers)
+        .catch(() => setErrorMessage('Try again later'))
+        .finally(() => setLoading(false))
+    }, 1000);
+  }, [updateAt])
+
+  function reload() {
+    setUpdateAt(new Date());
+    setErrorMessage('');
+  }
 
   return (
     <div className="section py-5">
       <div className="box">
         <div className="title">Users</div>
 
-        <UsersList users={users} />
-        <Blocks
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="blocks-loading"
-          wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"
-        />
+        {loading && (
+          <Blocks
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+          />
+        )}
 
-        <p className="title is-5">There are no users</p>
-        <p className="notification is-danger">Try again later</p>
+        {!loading && users.length > 0 && (
+          <UsersList users={users} />
+        )}
+
+        {!loading && !errorMessage && users.length === 0 && (
+          <p className="title is-5">There are no users</p>
+        )}
+
+        {errorMessage && (
+          <p className="notification is-danger">
+            {errorMessage}
+            <button onClick={reload}>Reload</button>
+          </p>
+        )}
       </div>
     </div>
   );
